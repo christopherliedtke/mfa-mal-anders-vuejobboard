6d58dc766cdd92cedb4cff6c7ec13d5223a81c01
christopherliedtke <ggwoods@gmx.de>
2020-07-29 14:41:20 +0200

    Clean up vue routes index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,33 +3,59 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 import store from "@/store";
 
+// Public
 import Home from "@/views/Home.vue";
 import Jobboard from "@/views/Jobboard.vue";
 import PublicJobView from "@/views/PublicJobView.vue";
-import Dashboard from "@/views/Dashboard.vue";
-import NewJobView from "@/views/NewJobView.vue";
-import EditJobView from "@/views/EditJobView.vue";
-import NewCompanyView from "@/views/NewCompanyView.vue";
-import EditCompanyView from "@/views/EditCompanyView.vue";
-import PrivateJobView from "@/views/PrivateJobView.vue";
-import Account from "@/views/Account.vue";
-import Admin from "@/views/Admin.vue";
-import AdminJobView from "@/views/AdminJobView.vue";
-import AdminEditJobView from "@/views/AdminEditJobView.vue";
-import AdminEditCompanyView from "@/views/AdminEditCompanyView.vue";
+import PrivacyPolicy from "@/views/PrivacyPolicy.vue";
+import Impressum from "@/views/Impressum.vue";
+
+// Auth
 import Login from "@/views/Login.vue";
 import Register from "@/views/Register.vue";
 import PasswordReset from "@/views/PasswordReset.vue";
 import AccountVerification from "@/views/AccountVerification.vue";
 import AccountVerificationSuccessful from "@/views/AccountVerificationSuccessful.vue";
-import PrivacyPolicy from "@/views/PrivacyPolicy.vue";
-import Impressum from "@/views/Impressum.vue";
+
+// Private
+import Account from "@/views/Account.vue";
+import Dashboard from "@/views/Dashboard.vue";
+
+import NewJobView from "@/views/NewJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
+import PrivateJobView from "@/views/PrivateJobView.vue";
+
+import NewCompanyView from "@/views/NewCompanyView.vue";
+import EditCompanyView from "@/views/EditCompanyView.vue";
+
+// Admin
+import Admin from "@/views/Admin.vue";
+import AdminJobView from "@/views/AdminJobView.vue";
+import AdminEditJobView from "@/views/AdminEditJobView.vue";
+import AdminEditCompanyView from "@/views/AdminEditCompanyView.vue";
 
 const routes = [
+    // #Public
     {
         path: "/",
         name: "Home",
         component: Home,
+        meta: {
+            public: true
+        }
+    },
+    {
+        path: "/jobboard",
+        name: "Jobboard",
+        component: Jobboard,
+        meta: {
+            public: true
+        }
+    },
+    {
+        path: "/jobboard/job/:jobId",
+        name: "PublicJobView",
+        component: PublicJobView,
         meta: {
             public: true
         }
@@ -50,20 +76,59 @@ const routes = [
             public: true
         }
     },
+    // #Auth
     {
-        path: "/jobboard",
-        name: "Jobboard",
-        component: Jobboard,
+        path: "/login",
+        name: "Login",
+        component: Login,
         meta: {
-            public: true
+            public: true,
+            onlyWhenLoggedOut: true
         }
     },
     {
-        path: "/jobboard/job/:jobId",
-        name: "PublicJobView",
-        component: PublicJobView,
+        path: "/register",
+        name: "Register",
+        component: Register,
         meta: {
-            public: true
+            public: true,
+            onlyWhenLoggedOut: true
+        }
+    },
+    {
+        path: "/password-reset",
+        name: "PasswordReset",
+        component: PasswordReset,
+        meta: {
+            public: true,
+            onlyWhenLoggedOut: true
+        }
+    },
+    {
+        path: "/account/verification",
+        name: "AccountVerification",
+        component: AccountVerification,
+        meta: {
+            public: false,
+            onlyWhenPending: true
+        }
+    },
+    {
+        path: "/account/verified",
+        name: "AccountVerificationSuccessful",
+        component: AccountVerificationSuccessful,
+        meta: {
+            public: false,
+            onlyWhenPending: true
+        }
+    },
+    // #Private
+    {
+        path: "/account",
+        name: "Account",
+        component: Account,
+        meta: {
+            public: false
         }
     },
     {
@@ -114,6 +179,7 @@ const routes = [
             public: false
         }
     },
+    // #Admin
     {
         path: "/admin",
         name: "Admin",
@@ -149,59 +215,6 @@ const routes = [
             public: false,
             onlyAdmin: true
         }
-    },
-    {
-        path: "/account",
-        name: "Account",
-        component: Account,
-        meta: {
-            public: false
-        }
-    },
-    {
-        path: "/account/verification",
-        name: "AccountVerification",
-        component: AccountVerification,
-        meta: {
-            public: false,
-            onlyWhenPending: true
-        }
-    },
-    {
-        path: "/account/verified",
-        name: "AccountVerificationSuccessful",
-        component: AccountVerificationSuccessful,
-        meta: {
-            public: false,
-            onlyWhenPending: true
-        }
-    },
-    {
-        path: "/login",
-        name: "Login",
-        component: Login,
-        meta: {
-            public: true,
-            onlyWhenLoggedOut: true
-        }
-    },
-    {
-        path: "/register",
-        name: "Register",
-        component: Register,
-        meta: {
-            public: true,
-            onlyWhenLoggedOut: true
-        }
-    },
-    {
-        path: "/password-reset",
-        name: "PasswordReset",
-        component: PasswordReset,
-        meta: {
-            public: true,
-            onlyWhenLoggedOut: true
-        }
     }
 ];
 
@@ -210,7 +223,7 @@ const router = new VueRouter({
     routes
 });
 
-// Check auth before entering routes
+// Checks before routes
 router.beforeEach((to, from, next) => {
     const isPublic = to.matched.some(record => record.meta.public);
     const onlyWhenLoggedOut = to.matched.some(
