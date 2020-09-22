942210286b5645c7e55090ba371a202a9d39e205
christopherliedtke <ggwoods@gmx.de>
2020-09-22 14:24:28 +0200

    Update AGBs for price model

--- client/src/views/public/pages/AGBs.vue
+++ client/src/views/public/pages/AGBs.vue
@@ -4,72 +4,77 @@
         <b-container class="privacy-policy py-5">
             <h2>I. Geltungsbereich</h2>
             <p>
-                Diese allgemeinen Gesch&auml;ftsbedingungen (AGB) gelten
-                f&uuml;r alle Vertr&auml;ge zwischen der Kristin Maurach,
-                Personalberatung und Recruiting, Mfa mal anders (nachfolgend
-                &bdquo;Mfa mal anders&ldquo; oder &bdquo;wir&ldquo; genannt) und
-                ihren Vertragspartnern (nachfolgend &bdquo;Kunden&ldquo;
+                Diese allgemeinen Geschäftsbedingungen (AGB) gelten für alle
+                Verträge zwischen der Kristin Maurach, Personalberatung und
+                Recruiting, Mfa mal anders (nachfolgend „Mfa mal anders“ oder
+                „wir“ genannt) und ihren Vertragspartnern (nachfolgend „Kunden“
                 genannt).
             </p>
             <p>
-                Gesch&auml;ftsbedingungen des Kunden oder Dritter finden keine
+                Geschäftsbedingungen des Kunden oder Dritter finden keine
                 Anwendung, auch wenn Mfa mal anders ihrer Geltung im Einzelfall
                 nicht gesondert widerspricht.
             </p>
             <h2>II. Kontaktdaten des Verantwortlichen</h2>
             <p>
-                Kristin Maurach<br />Personalberatung und Recruiting, Mfa mal
-                anders<br />Schreiberhauer Stra&szlig;e 9<br />10317 Berlin<br />E-Mail:
+                Kristin Maurach<br />
+                Personalberatung und Recruiting, Mfa mal anders<br />
+                Schreiberhauer Straße 9<br />
+                10317 Berlin<br />
+                E-Mail:
                 <a href="mailto:kontakt@mfa-mal-anders.de"
                     >kontakt@mfa-mal-anders.de</a
-                ><br />Telefon: 0176 63393957
+                ><br />
+                Telefon: 0176 63393957
             </p>
             <h2>III. Leistungen</h2>
             <p>
                 MFA mal anders stellt auf ihrer Webseite eine
-                Online-Jobb&ouml;rse zur Verf&uuml;gung. Arbeitgeber k&ouml;nnen
-                freie Stellen ver&ouml;ffentlichen oder Mfa mal anders mit der
+                Online-Stellenbörse zur Verfügung. Arbeitgeber können freie
+                Stellen veröffentlichen oder Mfa mal anders mit der
                 Personalsuche- und Beschaffung beauftragen.
             </p>
             <h2>IV. Vertragsabschluss</h2>
             <ol>
                 <li>
-                    F&uuml;r die Ver&ouml;ffentlichung einer Stellenanzeige des
-                    Kunden kommt ein Vertrag zustande, indem der Kunde das
-                    Online-Stellenbeschreibungsformular ausf&uuml;llt und den
-                    Buchen-Button sowie die AGBs per Klick best&auml;tigt. Der
-                    Kunde erh&auml;lt nach Auftragserteilung eine
-                    Auftragsbest&auml;tigung per E-Mail.
+                    Für die Veröffentlichung einer Stellenanzeige des Kunden
+                    kommt ein Vertrag zustande, indem der Kunde nach erfolgter
+                    Registrierung das Online-Formular zur Erstellung einer
+                    Stellenanzeige ausfüllt, die AGBs bestätigt und den
+                    Zahlungsprozess absolviert bzw. eine Rechnung für die
+                    Veröffentlichung der Stellenanzeige anfordert. Der Kunde
+                    erhält nach Auftragserteilung eine Auftragsbestätigung per
+                    E-Mail.
                 </li>
                 <li>
                     Sofern der Kunde Mfa mal anders mit der Personalsuche und
                     Rekrutierung beauftragt, wird ein gesonderter Vertrag in
-                    Papierform mit h&auml;ndischer Unterschrift beider Parteien
+                    Papierform mit händischer Unterschrift beider Parteien
                     (Kunde und Mfa mal anders) unterschrieben.
                 </li>
             </ol>
-            <h2>V. K&uuml;ndigung &amp; Vertragsbeendigung</h2>
+            <h2>V. Kündigung &amp; Vertragsbeendigung</h2>
             <ol>
                 <li>
                     Der Kunde kann innerhalb von 14 Tagen nach Erhalt der
-                    Auftragsbest&auml;tigung k&uuml;ndigen. Es kann in
-                    schriftlicher Form oder als Nachricht per E-Mail an
+                    Auftragsbestätigung kündigen. Es kann in schriftlicher Form
+                    oder als Nachricht per E-Mail an
                     <a href="mailto:kontakt@mfa-mal-anders.de"
                         >kontakt@mfa-mal-anders.de</a
                     >
-                    gek&uuml;ndigt werden.
+                    gekündigt werden.
                 </li>
                 <li>
-                    Der Vertrag gilt als beendet und erf&uuml;llt, wenn Mfa mal
+                    Der Vertrag gilt als beendet und erfüllt, wenn Mfa mal
                     anders die vom Kunden beauftragte Leistung erbracht hat.
                 </li>
             </ol>
             <h2>VI. Widerrufsrecht</h2>
             <p>
                 Der Kunde hat das Recht innerhalb von 14 Tagen ohne Angabe von
-                Gr&uuml;nden den Vertrag zu widerrufen. Dies muss in
-                schriftlicher Form per Post an die im Impressum angegebene
-                Adresse oder als Nachricht per E-Mail an
+                Gründen den Vertrag zu widerrufen. Dies muss in schriftlicher
+                Form per Post an die im Impressum angegebene Adresse oder als
+                Nachricht per E-Mail an
                 <a href="mailto:kontakt@mfa-mal-anders.de"
                     >kontakt@mfa-mal-anders.de</a
                 >
@@ -78,82 +83,81 @@
             <h2>VII. Preise</h2>
             <ol>
                 <li>
-                    Die Preise f&uuml;r die Ver&ouml;ffentlichung einer
-                    Stellenanzeige unterliegen dem Pay-what-you-want-Modell.
-                    D.h. der Kunde zahlt einen Betrag seiner Wahl, die er
-                    f&uuml;r die Leistung als angemessen ansieht. Eine
-                    m&ouml;gliche &Uuml;bersicht zur Preish&ouml;he findet sich
-                    auf der
-                    <a
-                        href="https://www.mfa-mal-anders.de/jobs-fuer-mfa/stellenschaltung/"
-                        >Webseite</a
-                    >.
+                    Die Preise für die Veröffentlichung einer Stellenanzeige
+                    unterliegen dem Pay-what-you-want-Modell, beinhalten jedoch
+                    einen Mindest-Betrag in Höhe von 25,00 Euro. D.h. der Kunde
+                    zahlt einen Betrag seiner Wahl ab 25,00 Euro, die er für die
+                    Leistung als angemessen ansieht. Eine mögliche Übersicht zur
+                    Preishöhe findet sich auf der
+                    <b-link to="/page/fuer-arbeitgeber">Webseite</b-link>.
                 </li>
                 <li>
-                    Gem&auml;&szlig; &sect; 19 UStG erheben wir keine
-                    Umsatzsteuer und weisen diese folglich auch nicht aus. Die
-                    Preise sind Endpreise.
+                    Gemäß § 19 UStG erheben wir keine Umsatzsteuer und weisen
+                    diese folglich auch nicht aus. Die Preise sind Endpreise.
                 </li>
             </ol>
-            <h2>VIII. Zahlungsmethoden und F&auml;lligkeit der Zahlung</h2>
+            <h2>VIII. Zahlungsmethoden und Fälligkeit der Zahlung</h2>
             <ol>
                 <li>
-                    Bei Auftrag zur Ver&ouml;ffentlichung einer Stellenanzeige:
-                    Der Kunde erh&auml;lt nach Ver&ouml;ffentlichung der
-                    Stellenanzeige eine Auftragsbest&auml;tigung per Email.
+                    Bei Auftrag zur Veröffentlichung einer Stellenanzeige: Der
+                    Kunde erhält nach erfolgter Zahlung einen Beleg für die
+                    erteilte Dienstleistung per Email.
                 </li>
                 <li>
-                    Den Preis w&auml;hlt der Kunde nach dem Pay-what-you-want
-                    Prinzip selbst. Zahlbar ist der Betrag innerhalb von 7 Tagen
-                    nach Ver&ouml;ffentlichung der Anzeige an unsere
-                    Bankverbindung.
+                    Den Preis zahlt der Kunde über eine der angebotenen
+                    Zahlungsmethoden über die sichere Stripe Zahlungsplattform
+                    (Kreditkarte, GiroPay, ggfls. Klarna Rechnung) auf der
+                    Webseite. Im Falle einer Zahlung per Kreditkarte oder
+                    GiroPay wird der gewählte Betrag dem Konto des Kunden
+                    unmittelbar belastet. Im Falle einer gesonderten Zahlung per
+                    Rechnung ist der Betrag innerhalb von 14 Tagen an das
+                    angegebene Konto zu überweisen.
                 </li>
                 <li>
-                    Nach Zahlungseingang erh&auml;lt der Kunde eine Rechnung per
-                    E-Mail an die vom Kunden bei der Auftragserteilung
-                    verwendete E-Mail-Adresse.
+                    Nach Zahlungseingang erhält der Kunde einen Beleg per E-Mail
+                    an die vom Kunden bei der Auftragserteilung verwendete
+                    E-Mail-Adresse.
                 </li>
                 <li>
                     Kommt der Kunde mit der Zahlung um mehr als 10 Tage in
-                    Verzug, so behalten wir uns vor, die ver&ouml;ffentlichte
+                    Verzug, so behalten wir uns vor, die veröffentlichte
                     Stellenanzeige bis zum Zahlungseingang nach vorheriger
                     Androhung und Fristsetzung von 7 Tagen zu sperren.
                 </li>
                 <li>
-                    Wird MFA mal anders f&uuml;r die Rekrutierung beauftragt,
-                    wird nur bei erfolgreicher Vermittlung eine Rechnung
-                    gestellt. Rechnungen sind ohne Abz&uuml;ge zahlbar innerhalb
-                    von 14 Tagen nach Rechnungsdatum an die angegebene
-                    Bankverbindung zu &uuml;berweisen.
+                    Wird MFA mal anders für die Rekrutierung beauftragt, wird
+                    nur bei erfolgreicher Vermittlung eine Rechnung gestellt.
+                    Rechnungen sind ohne Abzüge zahlbar innerhalb von 14 Tagen
+                    nach Rechnungsdatum an die angegebene Bankverbindung zu
+                    überweisen.
                 </li>
             </ol>
             <h2>IX. Rechte und Pflichten des Kunden</h2>
             <ol>
                 <li>
-                    Der Kunde ist f&uuml;r den Inhalt der geschalteten
-                    Stellenanzeige verantwortlich. Er hat sicherzustellen, dass
-                    keine sitten- oder-rechtswidrige Inhalte ver&ouml;ffentlicht
-                    werden. Au&szlig;erdem muss der Kunde sicher gehen, dass
-                    Text- und Bildrechte Dritter ausgeschlossen sind. Es besteht
-                    von Seiten Mfa mal anders keine Verpflichtung, die Anzeigen
-                    auf die Beeintr&auml;chtigung der Rechte Dritter zu
-                    &uuml;berpr&uuml;fen.
+                    Der Kunde ist für den Inhalt der geschalteten Stellenanzeige
+                    verantwortlich. Er hat sicherzustellen, dass keine sitten-
+                    oder-rechtswidrige Inhalte veröffentlicht werden. Außerdem
+                    muss der Kunde sicher tellen, dass Text- und Bildrechte
+                    Dritter ausgeschlossen sind. Es besteht von Seiten Mfa mal
+                    anders keine Verpflichtung, die Anzeigen auf die
+                    Beeinträchtigung der Rechte Dritter zu überprüfen.
                 </li>
                 <li>
                     Erstellt Mfa mal anders im Namen des Kunden eine
                     Stellenanzeige, ist der Kunde in der Pflicht, die Anzeige
-                    sofort nach Erhalt auf M&auml;ngel zu kontrollieren. Werden
-                    M&auml;ngel festgestellt, meldet der Kunde diese sofort
+                    sofort nach Erhalt auf Mängel zu kontrollieren. Werden
+                    Mängel festgestellt, meldet der Kunde diese sofort
                     schriftlich, per Email oder telefonisch an Mfa mal anders.
-                    Werden innerhalb von 14 Tagen keine M&auml;ngel gemeldet,
-                    gilt der Auftrag als erbracht im Sinne des Vertrags.
+                    Werden innerhalb von 7 Tagen keine Mängel gemeldet, gilt der
+                    Auftrag als erbracht im Sinne des Vertrags.
                 </li>
                 <li>
-                    Der Kunde erkl&auml;rt sich damit einverstanden, dass Mfa
-                    mal anders zu Zwecken der gr&ouml;&szlig;eren Reichweite der
+                    Der Kunde erklärt sich damit einverstanden, dass Mfa mal
+                    anders zu Zwecken der größeren Reichweite der
                     Stellenanzeige, diese auf Partnerportalen und Social Media
                     Plattformen ohne gesonderte Einwilligung des Kunden
-                    ver&ouml;ffentlicht werden d&uuml;rfen.
+                    veröffentlichen kann.
                 </li>
             </ol>
             <h2>X. Rechte und Pflichten von Mfa mal anders</h2>
@@ -162,12 +166,11 @@
                 solange in ihrem Eigentum sowie im Sinne des Urheberrechts, bis
                 der Kunde die Leistung bezahlt hat.
             </p>
-            <h2>XI. Gew&auml;hrleistung/Haftung</h2>
+            <h2>XI. Gewährleistung/Haftung</h2>
             <p>
-                Mfa mal anders &uuml;bernimmt keine Haftung f&uuml;r etwaige
-                Sch&auml;den und nicht eingetretene Erwartungen, die
-                au&szlig;erhalb des Einflusses von uns liegen. Ausnahme bilden
-                Nachbesserungen.
+                Mfa mal anders übernimmt keine Haftung für etwaige Schäden und
+                nicht eingetretene Erwartungen, die außerhalb des Einflusses von
+                uns liegen. Ausnahme bilden Nachbesserungen.
             </p>
             <h2>XII. Vertraulichkeit</h2>
             <p>
@@ -177,37 +180,34 @@
                 Daten werden nicht an Dritte weitergegeben.
             </p>
             <p>
-                Im Gegenzug verpflichtet sich Mfa mal anders, s&auml;mtliche ihr
-                w&auml;hrend der Zusammenarbeit mit dem Kunden bekannt
-                gewordenen Informationen vertraulich zu behandeln.
+                Im Gegenzug verpflichtet sich Mfa mal anders, sämtliche ihr
+                während der Zusammenarbeit mit dem Kunden bekannt gewordenen
+                Informationen vertraulich zu behandeln.
             </p>
             <p>
-                Der Kunde erkl&auml;rt sich einverstanden, dass bei der
-                &Uuml;bertragung von Daten im Internet f&uuml;r beide Parteien
-                nach dem derzeitigen Stand der Technik nicht ausgeschlossen
-                werden kann, dass sich Unbefugte w&auml;hrend des
-                &Uuml;bermittlungsvorgangs Zugriff auf die &uuml;bermittelten
-                Daten verschaffen.
+                Der Kunde erklärt sich einverstanden, dass bei der Übertragung
+                von Daten im Internet für beide Parteien nach dem derzeitigen
+                Stand der Technik nicht ausgeschlossen werden kann, dass sich
+                Unbefugte während des Übermittlungsvorgangs Zugriff auf die
+                übermittelten Daten verschaffen.
             </p>
             <h2>XIII. Schlussbestimmungen</h2>
             <p>
                 Die Rechtsbeziehung zwischen den Vertragsparteien unterliegen
-                ausschlie&szlig;lich dem Recht der Bundesrepublik Deutschland.
-                Der Kunde darf auf diesem Vertrag beruhende Anspr&uuml;che gegen
-                Mfa mal anders nur nach unserer Zustimmung auf Dritte
-                &uuml;bertragen.
+                ausschließlich dem Recht der Bundesrepublik Deutschland. Der
+                Kunde darf auf diesem Vertrag beruhende Ansprüche gegen Mfa mal
+                anders nur nach unserer Zustimmung auf Dritte übertragen.
             </p>
             <p>
                 Sollten eine oder mehrere Bestimmungen dieser AGB oder des
                 Vertrages, dem sie zugrunde liegen, unwirksam sein oder werden,
-                so wird dadurch die G&uuml;ltigkeit der &uuml;brigen
-                Bestimmungen nicht ber&uuml;hrt. Die unwirksame(n)
-                Bestimmung(en) werden vielmehr im Wege der erg&auml;nzenden
-                Vertragsauslegung durch eine solche Regelung ersetzt, die dem
-                von den Vertragsparteien mit der/den unwirksamen Bestimmung(en)
-                erkennbar verfolgten wirtschaftlichen Zweck m&ouml;glichst nahe
-                kommt. Entsprechendes gilt f&uuml;r die Ausf&uuml;llung etwaiger
-                Regelungsl&uuml;cken.
+                so wird dadurch die Gültigkeit der übrigen Bestimmungen nicht
+                berührt. Die unwirksame(n) Bestimmung(en) werden vielmehr im
+                Wege der ergänzenden Vertragsauslegung durch eine solche
+                Regelung ersetzt, die dem von den Vertragsparteien mit der/den
+                unwirksamen Bestimmung(en) erkennbar verfolgten wirtschaftlichen
+                Zweck möglichst nahe kommt. Entsprechendes gilt für die
+                Ausfüllung etwaiger Regelungslücken.
             </p>
         </b-container>
         <Head :title="title" desc="This is the meta of AGBs..." img="" />
