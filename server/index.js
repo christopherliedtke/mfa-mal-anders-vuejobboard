def971ecbdad096a6e7cfed1ca0606bbc6a25941
christopherliedtke <ggwoods@gmx.de>
2020-09-27 16:08:04 +0200

    Add dirname to * route

--- server/index.js
+++ server/index.js
@@ -78,6 +78,6 @@ app.use("/api/download", require("./routes/download"));
 // app.use("/", require("./routes/index"));
 
 // Serve the built static files in production
-app.get("*", (req, res) => res.sendFile("/index.html"));
+app.get("*", (req, res) => res.sendFile(__dirname + "/index.html"));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
