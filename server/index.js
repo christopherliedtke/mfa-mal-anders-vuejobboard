f70fe54bee73e90c7f3964370a7b601ff6200ade
christopherliedtke <ggwoods@gmx.de>
2020-09-27 16:14:49 +0200

    * to /public/index.html

--- server/index.js
+++ server/index.js
@@ -78,6 +78,6 @@ app.use("/api/download", require("./routes/download"));
 app.use("/", require("./routes/index"));
 
 // Serve the built static files in production
-app.get("*", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("*", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
