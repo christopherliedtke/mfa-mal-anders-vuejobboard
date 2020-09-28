71e5e5bbf382e77e698ce70d86d3a528da46abf5
christopherliedtke <ggwoods@gmx.de>
2020-09-28 11:07:19 +0200

    Remove redirects

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 });
 
 // #Redirections
-app.use("*", require("./utils/middleware/redirect"));
+// app.use("*", require("./utils/middleware/redirect"));
 
 // #Routes w/o csrf protection
 app.use("/api/webhooks", require("./routes/webhooks"));
