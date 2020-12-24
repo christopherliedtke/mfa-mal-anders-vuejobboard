816c7ef8463d9b30ba78d7c04b8b7a9ed3f8063f
christopherliedtke <ggwoods@gmx.de>
2020-12-24 15:41:40 +0100

    add deploy scripts

---
+++ server/public/js/chunk-2d0b2d49.4f6922f5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b2d49"],{"267f":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},r=[],a={name:"Page"},c=a,l=t("2877"),o=Object(l["a"])(c,u,r,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d0b2d49.4f6922f5.js.map
