f7a5380920584d0cb2e64dd2bdce676216a5abbd
christopherliedtke <ggwoods@gmx.de>
2021-02-25 18:06:58 +0100

    Fix deleteAccountBtn

--- client/src/components/buttons/DeleteAccountButton.vue
+++ client/src/components/buttons/DeleteAccountButton.vue
@@ -17,45 +17,41 @@
             footer-class="d-flex justify-content-between"
             @ok="deleteAccount"
         >
-            <b-overlay
-                :show="showOverlay"
-                variant="transparent"
-                blur="none"
-                z-index="9999"
-            >
-                <p class="my-4">
-                    Bist Du sicher, dass Du Deinen Account löschen möchtest?
-                </p>
-                <b-form>
-                    <label for="password">Passwort</label>
-                    <b-input-group>
-                        <b-form-input
-                            :type="passwordType"
-                            v-model="password"
-                            id="password"
-                            placeholder="Passwort eingeben..."
-                            autocomplete="current-password"
-                        ></b-form-input>
-                        <b-input-group-append is-text>
-                            <Fa
-                                :icon="
-                                    passwordType === 'text'
-                                        ? 'eye'
-                                        : 'eye-slash'
-                                "
-                                font-scale="1.2"
-                                @click="togglePasswordType"
-                            />
-                        </b-input-group-append>
-                    </b-input-group>
+            <p class="my-4">
+                Bist Du sicher, dass Du Deinen Account löschen möchtest?
+            </p>
+            <b-form>
+                <label for="password">Passwort</label>
+                <b-input-group>
+                    <b-form-input
+                        :type="passwordType"
+                        v-model="password"
+                        id="password"
+                        placeholder="Passwort eingeben..."
+                        autocomplete="current-password"
+                    ></b-form-input>
+                    <b-input-group-append is-text>
+                        <Fa
+                            :icon="
+                                passwordType === 'text' ? 'eye' : 'eye-slash'
+                            "
+                            font-scale="1.2"
+                            @click="togglePasswordType"
+                        />
+                    </b-input-group-append>
+                </b-input-group>
 
-                    <div class="error mt-3" v-if="error">
-                        <b-alert show dismissible variant="warning">{{
-                            error
-                        }}</b-alert>
-                    </div>
-                </b-form>
-            </b-overlay>
+                <div class="error mt-3">
+                    <b-alert
+                        v-for="error in errors"
+                        :key="error.message"
+                        show
+                        dismissible
+                        variant="warning"
+                        >{{ error.message }}</b-alert
+                    >
+                </div>
+            </b-form>
         </b-modal>
     </div>
 </template>
@@ -63,39 +59,44 @@
 <script>
     export default {
         name: "DeleteAccountButton",
+        data() {
+            return {
+                password: "",
+                passwordType: "password",
+                errors: null
+            };
+        },
         methods: {
             async deleteAccount(e) {
                 e.preventDefault();
-                this.showOverlay = true;
-                this.error = null;
 
-                const response = await this.$axios.post(
-                    "/api/auth/delete-account",
-                    {
-                        password: this.password
-                    }
-                );
-                this.showOverlay = false;
+                this.$store.dispatch("setOverlay", true);
 
-                if (!response.data.success) {
-                    this.error = response.data.error;
+                this.errors = null;
+
+                const response = await this.$axios.post("/graphql", {
+                    query: `
+                        mutation {
+                            deleteMe (password: "${this.password}") {
+                                _id
+                            }
+                        }
+                    `
+                });
+
+                if (response.data.errors) {
+                    this.errors = response.data.errors;
                 } else {
                     this.$store.dispatch("logout");
                 }
+
+                this.$store.dispatch("setOverlay", false);
             },
             togglePasswordType() {
                 this.passwordType === "text"
                     ? (this.passwordType = "password")
                     : (this.passwordType = "text");
             }
-        },
-        data() {
-            return {
-                password: "",
-                passwordType: "password",
-                showOverlay: false,
-                error: null
-            };
         }
     };
 </script>
