e4759a1ff4c3d647ff1684b17a9e6264c93b5370
christopherliedtke <ggwoods@gmx.de>
2021-04-18 18:32:58 +0200

    Update to preload instead of prefetch chunks

--- client/vue.config.js
+++ client/vue.config.js
@@ -27,6 +27,20 @@ module.exports = {
     //             }
     //         ]);
     // }
+    /* 
+       Disable (or customize) prefetch, see:
+       https://cli.vuejs.org/guide/html-and-static-assets.html#prefetch
+    */
+    config.plugins.delete("prefetch");
+
+    /* 
+       Configure preload to load all chunks
+       NOTE: use `allChunks` instead of `all` (deprecated)
+    */
+    config.plugin("preload").tap(options => {
+      options[0].include = "allChunks";
+      return options;
+    });
     config.module
       .rule("vue")
       .use("vue-loader")
