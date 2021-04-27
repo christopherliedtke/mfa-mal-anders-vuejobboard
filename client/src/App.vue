a3c5d961c4b4e360db50c63266cd8a9be6b3e845
christopherliedtke <ggwoods@gmx.de>
2021-04-27 13:04:07 +0200

    Update lazy loads in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,8 +13,14 @@
 <script>
   import Header from "@/components/layout/Header";
   import Footer from "@/components/layout/Footer";
-  import NewsletterSignUpPopUp from "@/components/popups/NewsletterSignUpPopUp.vue";
-  import Overlay from "@/components/utils/Overlay";
+  // import NewsletterSignUpPopUp from "@/components/popups/NewsletterSignUpPopUp.vue";
+  const NewsletterSignUpPopUp = () =>
+    import(
+      /* webpackChunkName: "newsletterSignUpPopUp" */ "@/components/popups/NewsletterSignUpPopUp.vue"
+    );
+  // import Overlay from "@/components/utils/Overlay";
+  const Overlay = () =>
+    import(/* webpackChunkName: "overlay" */ "@/components/utils/Overlay");
   export default {
     components: {
       Header,
