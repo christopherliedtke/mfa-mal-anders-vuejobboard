e83bfb3394ceadb2a6aa7b49fdbf13008d8adb93
christopherliedtke <ggwoods@gmx.de>
2021-05-15 14:29:20 +0200

    Added geoCodeSorting to publicJobs; Add optional external jobs to publicJobs

--- server/routes/publicJobs.js
+++ server/routes/publicJobs.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const config = require("../config/config");
 const internalJobsCache = require("../cache/internalJobsCache");
+const jobLiftCache = require("../cache/jobLiftCache");
+const stepstoneCache = require("../cache/stepstoneCache");
 
 // #route:  GET /api/public-jobs
 // #desc:   Fetch public jobs
@@ -9,7 +12,14 @@ router.get("/", async (req, res) => {
   const { query } = req;
   console.log("query: ", query);
 
-  let jobs = await internalJobsCache.get("jobs");
+  // let jobs = await internalJobsCache.get("jobs");
+  let jobs = await Promise.all([
+    internalJobsCache.get("jobs"),
+    config.externalJobs.joblift ? jobLiftCache.get("jobs") : [],
+    config.externalJobs.stepstone ? stepstoneCache.get("jobs") : [],
+  ]);
+  jobs = jobs.flat();
+  // console.log("jobs: ", jobs);
 
   jobs = filterJobs(jobs, query);
   jobs = sliceJobs(jobs, query.limit || undefined, query.offset || undefined);
@@ -19,22 +29,15 @@ router.get("/", async (req, res) => {
 
 module.exports = router;
 
-function filterJobs(
-  jobs,
-  query
-  //   s = "",
-  //   location = "",
-  //   state = "",
-  //   employmentType = "",
-  //   profession = "",
-  //   specialization = ""
-) {
+function filterJobs(jobs, query) {
   let filteredJobs = [...jobs];
 
   //   filter profession
   if (query.profession) {
     filteredJobs = filteredJobs.filter(
-      (job) => job.profession.toLowerCase() === query.profession.toLowerCase()
+      (job) =>
+        job.profession &&
+        job.profession.toLowerCase() === query.profession.toLowerCase()
     );
   }
 
@@ -42,30 +45,39 @@ function filterJobs(
   if (query.employmentType) {
     filteredJobs = filteredJobs.filter(
       (job) =>
+        job.employmentType &&
         job.employmentType.toLowerCase() === query.employmentType.toLowerCase()
     );
   }
 
   //   filter location
   if (query.location) {
-    filteredJobs = filteredJobs.filter((job) =>
-      job.company.location.toLowerCase().includes(query.location.toLowerCase())
+    filteredJobs = filteredJobs.filter(
+      (job) =>
+        job.company.location &&
+        job.company.location
+          .toLowerCase()
+          .includes(query.location.toLowerCase())
     );
   }
 
   //   filter state
   if (query.state) {
     filteredJobs = filteredJobs.filter(
-      (job) => job.company.state.toLowerCase() === query.state.toLowerCase()
+      (job) =>
+        job.company.state &&
+        job.company.state.toLowerCase() === query.state.toLowerCase()
     );
   }
 
   //   filter specialization
   if (query.specialization) {
-    filteredJobs = filteredJobs.filter((job) =>
-      query.specialization
-        .toLowerCase()
-        .includes(job.specialization.toLowerCase())
+    filteredJobs = filteredJobs.filter(
+      (job) =>
+        job.specialization &&
+        query.specialization
+          .toLowerCase()
+          .includes(job.specialization.toLowerCase())
     );
   }
 
@@ -88,9 +100,54 @@ function filterJobs(
     });
   }
 
+  // sort by geocode
+  if (query.geoCodeLat && query.geoCodeLng) {
+    filteredJobs = sortByDistance(
+      query.geoCodeLat,
+      query.geoCodeLng,
+      filteredJobs
+    );
+  }
+
   return filteredJobs;
 }
 
 function sliceJobs(jobs = [], limit = 25, offset = 0) {
   return jobs.slice(offset, offset + limit);
+}
+
+function sortByDistance(geoCodeLat, geoCodeLng, jobs) {
+  return jobs.sort((a, b) => {
+    return (
+      calcDistance(
+        a.company.geoCodeLat,
+        a.company.geoCodeLng,
+        geoCodeLat,
+        geoCodeLng
+      ) -
+      calcDistance(
+        b.company.geoCodeLat,
+        b.company.geoCodeLng,
+        geoCodeLat,
+        geoCodeLng
+      )
+    );
+  });
+}
+
+function calcDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371000; // metres
+  const φ1 = (lat1 * Math.PI) / 180; // φ, λ in radians
+  const φ2 = (lat2 * Math.PI) / 180;
+  const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+  const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const d = R * c; // in metres
+
+  return d;
 }
