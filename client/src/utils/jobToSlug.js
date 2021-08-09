09b4a7d39b92a8b4055c57e84748ec6e8a7a5426
christopherliedtke <ggwoods@gmx.de>
2021-08-09 15:33:04 +0200

    Updated string_to_slug for umlaute + ß

--- client/src/utils/jobToSlug.js
+++ client/src/utils/jobToSlug.js
@@ -4,14 +4,16 @@ export default function jobToSlug(title, location) {
   }
 
   return string_to_slug(title + " in " + location);
-  // .toLowerCase()
-  // .replace(/\s|\/|&|#|\?|,|:|;|"|_|\*/gi, "-")
-  // .replace(/-+/g, "-");
 }
 
 function string_to_slug(str) {
   str = str.replace(/^\s+|\s+$/g, ""); // trim
   str = str.toLowerCase();
+  str = str
+    .replace(/ß/g, "ss")
+    .replace(/ä/g, "ae")
+    .replace(/ö/g, "oe")
+    .replace(/ü/g, "ue"); // replace ß, ä, ö, ü
 
   // remove accents, swap ñ for n, etc
   var from = "àáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
