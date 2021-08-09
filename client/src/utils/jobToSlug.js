9c47f55e49f8c76c6f660a20797fb2a4c92fd679
christopherliedtke <ggwoods@gmx.de>
2021-08-09 14:16:25 +0200

    Updated string_to_slug function

--- client/src/utils/jobToSlug.js
+++ client/src/utils/jobToSlug.js
@@ -3,8 +3,28 @@ export default function jobToSlug(title, location) {
     return "";
   }
 
-  return (title + " in " + location)
-    .toLowerCase()
-    .replace(/ /gi, "-")
-    .replace(/\//gi, "-");
+  return string_to_slug(title + " in " + location);
+  // .toLowerCase()
+  // .replace(/\s|\/|&|#|\?|,|:|;|"|_|\*/gi, "-")
+  // .replace(/-+/g, "-");
+}
+
+function string_to_slug(str) {
+  str = str.replace(/^\s+|\s+$/g, ""); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to = "aaaaaeeeeiiiioooouuuunc------";
+
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+    .replace(/\s+/g, "-") // collapse whitespace and replace by -
+    .replace(/-+/g, "-"); // collapse dashes
+
+  return str;
 }
