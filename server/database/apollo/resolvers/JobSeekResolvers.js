cac68eccafcdb13d258295baf4ea3f3f20a1943e
christopherliedtke <ggwoods@gmx.de>
2021-12-12 13:08:24 +0100

    Updated myJobSeeks updateMany

--- server/database/apollo/resolvers/JobSeekResolvers.js
+++ server/database/apollo/resolvers/JobSeekResolvers.js
@@ -103,9 +103,12 @@ const JobSeekResolvers = {
         user: context.user._id,
       }).sort({ createdAt: "desc" });
 
-      await JobSeek.updateMany({
-        user: context.user._id,
-      });
+      await JobSeek.updateMany(
+        {
+          user: context.user._id,
+        },
+        { updatedAt: new Date().getTime() }
+      );
 
       return jobSeeks;
     },
--- server/database/apollo/resolvers/TrainingResolvers.js
+++ server/database/apollo/resolvers/TrainingResolvers.js
@@ -96,9 +96,12 @@ const TrainingResolvers = {
         user: context.user._id,
       }).sort({ createdAt: "desc" });
 
-      await Training.updateMany({
-        user: context.user._id,
-      });
+      await Training.updateMany(
+        {
+          user: context.user._id,
+        },
+        { updatedAt: new Date().getTime() }
+      );
 
       return trainings;
     },
