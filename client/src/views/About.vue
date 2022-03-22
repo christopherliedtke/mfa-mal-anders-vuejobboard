ffe979c93a407dfaefaffc2376ddb571acce5032
christopherliedtke <ggwoods@gmx.de>
2022-03-22 09:28:41 +0100

    Fixed typos; added whatsapp contact

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -168,31 +168,31 @@
           So erreichen Sie uns
         </p>
 
-        <div class="row row-cols-1 row-cols-md-3 mt-lg-5">
-          <div class="col mb-4">
+        <div class="row mt-lg-5">
+          <div class="col-12 col-lg-4 mb-4">
             <p>
               Bei Anfragen oder Hinweisen können Sie uns gern jederzeit
               kontaktieren. Wir freuen uns auf Ihre Nachricht.
             </p>
-            <p>
+            <!-- <p>
               <strong class="mr-3">E-Mail:</strong>
               <b-link href="mailto:kontakt@mfa-mal-anders.de" target="_blank"
                 >kontakt@mfa-mal-anders.de</b-link
               >
-            </p>
+            </p> -->
           </div>
-          <div class="col mb-4">
+          <div class="col-6 col-lg-2 mb-4">
             <b-link
-              class="d-flex flex-column align-items-center justify-content-center lead bold text-muted"
+              class="d-flex flex-column align-items-center justify-content-center bold text-muted"
               to="/kontakt"
             >
               <div
-                class="d-inline-block bg-secondary text-light rounded-circle shadow1 p-4 mb-4"
+                class="d-inline-block bg-secondary text-light rounded-circle shadow1 p-4 mb-3"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="50"
-                  height="50"
+                  width="35"
+                  height="35"
                   fill="currentColor"
                   class="bi bi-file-text"
                   viewBox="0 0 16 16"
@@ -208,19 +208,19 @@
               Kontaktformular
             </b-link>
           </div>
-          <div class="col mb-4">
+          <div class="col-6 col-lg-2 mb-4">
             <b-link
-              class="d-flex flex-column align-items-center justify-content-center lead bold text-muted"
+              class="d-flex flex-column align-items-center justify-content-center bold text-muted"
               href="mailto:kontakt@mfa-mal-anders.de"
               target="_blank"
             >
               <div
-                class="d-inline-block bg-secondary text-light rounded-circle shadow1 p-4 mb-4"
+                class="d-inline-block bg-secondary text-light rounded-circle shadow1 p-4 mb-3"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="50"
-                  height="50"
+                  width="35"
+                  height="35"
                   fill="currentColor"
                   class="bi bi-envelope-fill"
                   viewBox="0 0 16 16"
@@ -231,6 +231,57 @@
                 </svg>
               </div>
               E-Mail
+            </b-link>
+          </div>
+          <div class="col-6 col-lg-2 mb-4">
+            <b-link
+              class="d-flex flex-column align-items-center justify-content-center bold text-muted"
+              href="https://wa.me/4917645282858"
+              target="_blank"
+            >
+              <div
+                class="d-inline-block bg-secondary text-light rounded-circle shadow1 p-4 mb-3"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="35"
+                  height="35"
+                  fill="currentColor"
+                  class="bi bi-whatsapp"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"
+                  />
+                </svg>
+              </div>
+              WhatsApp
+            </b-link>
+          </div>
+          <div class="col-6 col-lg-2 mb-4">
+            <b-link
+              class="d-flex flex-column align-items-center justify-content-center bold text-muted"
+              href="tel:+4917645282858"
+              target="_blank"
+            >
+              <div
+                class="d-inline-block bg-secondary text-light rounded-circle shadow1 p-4 mb-3"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="35"
+                  height="35"
+                  fill="currentColor"
+                  class="bi bi-phone"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"
+                  />
+                  <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                </svg>
+              </div>
+              Telefon
             </b-link>
           </div>
         </div>
