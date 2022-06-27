e2bfdccaa8e291abd4f3d46627084e1868ac19ac
Christopher Liedtke <ggwoods@gmx.de>
2022-06-27 08:54:11 +0200

    Updatred recachePrerender w/ trycatch

--- server/lib/recachePrerender.js
+++ server/lib/recachePrerender.js
@@ -2,10 +2,14 @@ const axios = require("axios");
 
 const recachePrerender = url => {
   if (process.env.PRERENDER_ACTIVE === "on") {
-    axios.post("https://api.prerender.io/recache", {
-      prerenderToken: process.env.PRERENDER_TOKEN,
-      url: url,
-    });
+    try {
+      axios.post("https://api.prerender.io/recache", {
+        prerenderToken: process.env.PRERENDER_TOKEN,
+        url: url,
+      });
+    } catch (error) {
+      console.error("Error on recachePrerender(): ", error);
+    }
   }
 };
 
