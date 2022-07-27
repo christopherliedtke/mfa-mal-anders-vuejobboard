0a7ae745df60df0f4162f45410da6d790958ee2d
Christopher Liedtke <ggwoods@gmx.de>
2022-07-27 20:49:40 +0200

    Updated recachePrerender w/ async await

--- server/lib/recachePrerender.js
+++ server/lib/recachePrerender.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 
-const recachePrerender = url => {
+const recachePrerender = async url => {
   if (process.env.PRERENDER_ACTIVE === "on") {
     try {
-      axios.post("https://api.prerender.io/recache", {
+      await axios.post("https://api.prerender.io/recache", {
         prerenderToken: process.env.PRERENDER_TOKEN,
         url: url,
       });
