54ea58cd6b2c957a8a8a485425e00ec13255c6aa
Christopher Liedtke <ggwoods@gmx.de>
2022-10-06 14:54:31 +0200

    Updated numbers and facts; updated employer hero

--- client/src/components/TheNumbersAndFacts.vue
+++ client/src/components/TheNumbersAndFacts.vue
@@ -15,7 +15,7 @@
           />
         </svg>
         <div class="display-4 bold mb-3">
-          710.000+
+          810.000+
         </div>
         <p>Menschen sehen uns monatlich bei Google</p>
       </div>
@@ -25,17 +25,18 @@
           width="60"
           height="60"
           fill="currentColor"
-          class="bi bi-facebook text-secondary mt-3 mb-4"
+          class="bi bi-meta text-secondary mt-3 mb-4"
           viewBox="0 0 16 16"
         >
           <path
-            d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"
+            fill-rule="evenodd"
+            d="M8.217 5.243C9.145 3.988 10.171 3 11.483 3 13.96 3 16 6.153 16.001 9.907c0 2.29-.986 3.725-2.757 3.725-1.543 0-2.395-.866-3.924-3.424l-.667-1.123-.118-.197a54.944 54.944 0 0 0-.53-.877l-1.178 2.08c-1.673 2.925-2.615 3.541-3.923 3.541C1.086 13.632 0 12.217 0 9.973 0 6.388 1.995 3 4.598 3c.319 0 .625.039.924.122.31.086.611.22.913.407.577.359 1.154.915 1.782 1.714Zm1.516 2.224c-.252-.41-.494-.787-.727-1.133L9 6.326c.845-1.305 1.543-1.954 2.372-1.954 1.723 0 3.102 2.537 3.102 5.653 0 1.188-.39 1.877-1.195 1.877-.773 0-1.142-.51-2.61-2.87l-.937-1.565ZM4.846 4.756c.725.1 1.385.634 2.34 2.001A212.13 212.13 0 0 0 5.551 9.3c-1.357 2.126-1.826 2.603-2.581 2.603-.777 0-1.24-.682-1.24-1.9 0-2.602 1.298-5.264 2.846-5.264.091 0 .181.006.27.018Z"
           />
         </svg>
         <div class="display-4 bold mb-3">
-          30.000+
+          125.000+
         </div>
-        <p>Monatliche Reichweite bei Facebook</p>
+        <p>Monatliche Reichweite bei Facebook & Instagram</p>
       </div>
       <div class="col text-center">
         <svg
@@ -53,12 +54,12 @@
           ></path>
         </svg>
         <div class="display-4 bold mb-3">
-          111.000+
+          115.000+
         </div>
         <p>Monatliche Seitenaufrufe auf der Webseite</p>
       </div>
     </div>
-    <p class="mt-4 small text-muted text-right">Stand: 01/2022</p>
+    <p class="mt-4 small text-muted text-right">Stand: 09/2022</p>
   </div>
 </template>
 
--- client/src/views/Employer.vue
+++ client/src/views/Employer.vue
@@ -73,8 +73,24 @@
                 <span class="text-secondary">Engagierte MFA & ZFA</span>
                 erreichen und exklusiv direkten
                 <b-link to="/stellengesuche" class="text-secondary"
-                  >Zugriff auf unsere Stellensuchenden</b-link
-                >
+                  >Zugriff auf unsere Stellensuchenden<svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    fill="currentColor"
+                    class="bi bi-box-arrow-up-right position-relative"
+                    style="top: -0.3em; margin-left: 2px"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+                    /></svg
+                ></b-link>
                 erhalten
               </div>
             </li>
@@ -120,8 +136,24 @@
                 <span class="">Mehr passende KandidatInnen</span>
                 per optionaler
                 <b-link to="#kurzbewerbung" class="text-secondary"
-                  >2&#8209;Minuten&nbsp;Kurzbewerbung</b-link
-                >
+                  >2&#8209;Minuten&nbsp;Kurzbewerbung<svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    fill="currentColor"
+                    class="bi bi-box-arrow-up-right position-relative"
+                    style="top: -0.3em; margin-left: 2px"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+                    /></svg
+                ></b-link>
               </div>
             </li>
             <li class="d-flex mb-2">
@@ -297,12 +329,11 @@
               Mit unserer optionalen
               <b-link to="#kurzbewerbung"><strong>Kurzbewerbung</strong></b-link
               >-Funktion senken Sie die Hürde zum Abschicken einer Bewerbung und
-              <strong>vervielfachen</strong> in der Regel
-              <strong>die Anzahl passender KandidatInnen</strong>.
+              <strong>vervielfachen die Anzahl passender KandidatInnen</strong>.
             </p>
           </div>
         </div>
-        <div class="col">
+        <!-- <div class="col">
           <div class="icon-box shadow1 py-5 px-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -326,7 +357,7 @@
               anbieten.
             </p>
           </div>
-        </div>
+        </div> -->
         <div class="col">
           <div class="icon-box shadow1 py-5 px-4">
             <svg
